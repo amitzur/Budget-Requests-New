ea491e849bf2e2d053814af74014bfdd33435a61
Amit Zur <amit@capriza.com>
2012-04-09 22:17:59 +0300

    log facebook auth

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,5 +1,6 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
+    puts request.env["omniauth.auth"]
     @user = User.find_for_facebook_oauth(request.env["omniauth.auth"], current_user)
 
     if @user.persisted?
