fdb8c47fe2f30f063ccd0cd1cc9a255dfa63d2d5
Amit Zur <amit@capriza.com>
2012-04-26 16:35:24 +0300

    precompile issure

--- config/application.rb
+++ config/application.rb
@@ -56,6 +56,6 @@ module Ksafim
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    config.assets.initialize_on_precompile = true
+    config.assets.initialize_on_precompile = false
   end
 end
